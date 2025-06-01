955397574dfd5dfd405134a64519f6072ad41776
DominikGalenza <dominik.galenza@gmail.com>
2025-06-01 13:25:13 +0200

    Created widget stack gameplay tags.

---
+++ Source/AdvancedUI/Public/FrontendGameplayTags.h
@@ -0,0 +1,13 @@
+// Vince Petrelli All Rights Reserved
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace FrontendGameplayTags
+{
+	ADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_Modal);
+	ADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_GameMenu);
+	ADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_GameHud);
+	ADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_Frontend);
+}
