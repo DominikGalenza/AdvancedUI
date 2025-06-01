d1c735be8df38b8d4e4f56caa61b304c85cdefb1
DominikGalenza <dominik.galenza@gmail.com>
2025-06-01 12:11:11 +0200

    Created frontend player controller.

---
+++ Source/AdvancedUI/Public/Controllers/FrontendPlayerController.h
@@ -0,0 +1,17 @@
+// Vince Petrelli All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "FrontendPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ADVANCEDUI_API AFrontendPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
