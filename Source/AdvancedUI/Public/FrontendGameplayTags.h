e3cfb4ede9eeadf0002e7a17417814a1a8f65410
DominikGalenza <dominik.galenza@gmail.com>
2025-06-05 14:44:03 +0200

    Created custom developer settings for referencing widgets.

--- Source/AdvancedUI/Public/FrontendGameplayTags.h
+++ Source/AdvancedUI/Public/FrontendGameplayTags.h
@@ -6,8 +6,13 @@
 
 namespace FrontendGameplayTags
 {
+	//Frontend widget stack
 	ADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_Modal);
 	ADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_GameMenu);
 	ADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_GameHud);
 	ADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_Frontend);
+
+	//Frontend widgets
+	ADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_Widget_PressAnyKeyScreen);
+	ADVANCEDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_Widget_MainMenuScreen);
 }
---
+++ Source/AdvancedUI/Public/FrontendSettings/FrontendDeveloperSettings.h
@@ -0,0 +1,23 @@
+// Vince Petrelli All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"
+#include "GameplayTagContainer.h"
+#include "FrontendDeveloperSettings.generated.h"
+
+class UWidget_ActivatableBase;
+
+/**
+ * 
+ */
+UCLASS(Config = Game, defaultconfig, meta = (DisplayName = "Frontend UI Settings"))
+class ADVANCEDUI_API UFrontendDeveloperSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(Config, EditAnywhere, Category = "Widget Reference", meta = (ForceInlineRow, Categories = "Frontend.Widget"))
+	TMap<FGameplayTag, TSoftClassPtr<UWidget_ActivatableBase>> FrontendWidgetMap;
+};
