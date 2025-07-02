2edb9367665bd04e571fab8e20f56497c30275ee
DominikGalenza <dominik.galenza@gmail.com>
2025-07-02 16:39:17 +0200

    Added bound action buttons to the options screen.

--- Source/AdvancedUI/Public/Widgets/Options/Widget_OptionsScreen.h
+++ Source/AdvancedUI/Public/Widgets/Options/Widget_OptionsScreen.h
@@ -13,5 +13,18 @@ UCLASS(Abstract, BlueprintType, meta = (DisableNaiveTick))
 class ADVANCEDUI_API UWidget_OptionsScreen : public UWidget_ActivatableBase
 {
 	GENERATED_BODY()
-	
+
+protected:
+	//~ Begin UUserWidget interface
+	virtual void NativeOnInitialized() override;
+	//~ End UUserWidget interface
+
+private:
+	void OnResetBoundActionTriggered();
+	void OnBackBoundActionTriggered();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Frontend Options Screen", meta = (RowType = "/Script/CommonUI.CommonInputActionDataBase"))
+	FDataTableRowHandle ResetAction;
+
+	FUIActionBindingHandle ResetActionHandle;
 };
